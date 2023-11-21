f9b50d8347ebc924e7f911abe011d73e02d0a4b2
stla <laurent_step@yahoo.fr>
2023-11-21 05:20:27 +0100

    third commit

---
+++ src/Geometry/ConvexHull.hs
@@ -0,0 +1,16 @@
+{-|
+Module      : Geometry.ConvexHull
+Description : Convex hull in arbitrary dimension.
+Copyright   : (c) Stéphane Laurent, 2023
+License     : GPL-3
+Maintainer  : laurent_step@outlook.fr
+
+See README for examples.
+-}
+module Geometry.ConvexHull
+  ( module X )
+  where
+import           Geometry.ConvexHull.ConvexHull as X
+import           Geometry.ConvexHull.Types      as X
+import           Geometry.Qhull.Shared          as X
+import           Geometry.Qhull.Types           as X
